3262899f401060a568ebcf07335eddca837de1e1
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2024-04-20 20:39:13 -0400

    add header menu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
-export default function Home() {
-  return <main>Something in here</main>;
+export default function Dashboard() {
+  return (
+      <main className="flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col gap-4 bg-muted/40 p-4 md:gap-8 md:p-10"></main>
+  );
 }
